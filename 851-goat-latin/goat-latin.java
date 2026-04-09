cbe1ed1c82477e50f3ab23bc4aae44ce533a0242
Srinadh Bejawada <2100030051cseh@gmail.com>
2026-04-09 14:09:20 +0530

    Time: 3 ms (57.91%) | Memory: 43.1 MB (82.00%) - LeetSync

---
+++ 851-goat-latin/goat-latin.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+
+        String sent[] = sentence.split(" ");
+
+        for(int i=0;i<sent.length;i++){
+            StringBuilder sb  = new StringBuilder(sent[i]);
+
+            char ch = sb.charAt(0);
+            if(ch == 'a' || ch=='e' || ch == 'i' || ch == 'o' || ch == 'u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch =='U'){
+                sb.append("ma");
+            }
+            else{
+                sb.deleteCharAt(0);
+                sb.append(ch+"ma");
+            }
+            sb.append("a".repeat(i+1)).append("");
+            sent[i] = sb.toString();
+        }
+        
+        return String.join(" ", sent);
+    }
+}
